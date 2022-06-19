c6a9b4c51f67ad50ae1f7bbe1a4225df087045e5
Nas <n.choudhury1@live.co.uk>
2022-06-19 22:57:57 +0100

    completed all files for submission

--- scripts/app_dom.js
+++ scripts/app_dom.js
@@ -1,1 +1,77 @@
-/* dom manipulation */
+/* dom manipulation and eventlistener */
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
+
+
+// update browser
+const updateUI = (data) => {
+
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = ` 
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // update day/night and icon img
+  let iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = null;
+  if(weather.IsDayTime) {
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  };
+  // set card day/night img
+  time.setAttribute('src', timeSrc);
+
+  // remove class d-none from card if of first instance
+  if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  };  
+};
+
+
+// get input from the city field
+const updateCity = async (city) => {
+  
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return {
+    // cityDetails: cityDetails,
+    // weather: weather
+
+    // using object shorthand notation
+    cityDetails,
+    weather
+  };
+};
+
+cityForm.addEventListener('submit', (e) => {
+
+  // prevent browser refreash
+  e.preventDefault();
+  
+  // input field value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => updateUI(err));
+
+});
+
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,2 +1,47 @@
-/* weather forecast api */
+/* weather forecast api interaction */
+
+// accaweather api key
 const key = '9lAH7qVa4pNH5KOUczDchpgIwOqGCmWr';
+
+// get city information
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+
+  // base url for weather database request made to
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+  // query parameter, set to apiKey & city code
+  const query = `?apikey=${key}&q=${city}`; 
+
+  // await return to base and query
+  const response = await fetch(base + query);
+  // convert returned json into readable data
+  const data = await response.json();
+
+  // rtn a promise
+  return data[0];
+};
+
+
+/* testing */
+
+// getCity('hackney')
+//   .then(data => {
+//     return getWeather(data.Key);
+//   }).then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => console.log(err));
+
+// getWeather(323250);
